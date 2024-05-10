1b5b1265abe1aaee8a5250c285d88285c9eff33c
amadarshprakash <adarshprakash456@gmail.com>
2024-05-10 17:51:26 +0530

    first commit

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,95 @@
+import React, { useRef, useState, useCallback, forwardRef, useImperativeHandle, useEffect } from 'react'
+import {
+    ViewerApp,
+    AssetManagerPlugin,
+    GBufferPlugin,
+    ProgressivePlugin,
+    TonemapPlugin,
+    SSRPlugin,
+    SSAOPlugin,
+    BloomPlugin,
+    GammaCorrectionPlugin,
+    mobileAndTabletCheck
+} from "webgi";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import { scrollAnimation } from '../lib/scroll-animation';
+
+gsap.registerPlugin(ScrollTrigger);
+const WebgiViewer = () => {
+    const canvasRef = useRef(null);
+
+    const memorizedScollAnimation = useCallback((
+        position,target,onUpdate
+    )=>{
+        if(position && target && onUpdate){
+            scrollAnimation(position,target,onUpdate)
+        }
+    }, []
+    )
+    const setupViewer = useCallback(async () => {
+        const viewer = new ViewerApp({
+            canvas: canvasRef.current,
+        })
+
+        // Add some plugins
+        const manager = await viewer.addPlugin(AssetManagerPlugin)
+        const camera = viewer.scene.activeCamera;
+        const position = camera.position;
+        const target = camera.target;
+        // Add a popup(in HTML) with download progress when any asset is downloading.
+        // await viewer.addPlugin(CanvasSnipperPlugin)
+
+        await viewer.addPlugin(GBufferPlugin)
+        await viewer.addPlugin(new ProgressivePlugin(32))
+        await viewer.addPlugin(new TonemapPlugin(true))
+        await viewer.addPlugin(GammaCorrectionPlugin)
+        await viewer.addPlugin(SSRPlugin)
+        await viewer.addPlugin(SSAOPlugin)
+        await viewer.addPlugin(BloomPlugin)
+        
+        viewer.renderer.refreshPipeline()
+
+        await manager.addFromPath('scene-black.glb');
+
+        viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
+        // Import and add a GLB file.
+
+       
+        viewer.scene.activeCamera.setCameraOptions({
+            controlsEnabled: false
+        })
+        window.scrollTo(0,0);
+
+        let needsUpdate = true; 
+        
+        const onUpdate = () =>{
+            needsUpdate = true;
+            viewer.setDirty();
+        }
+        viewer.addEventListener("preFrame", ()=>{
+if(needsUpdate){
+    camera.positionTargetUpdated(true)
+     needsUpdate = false;   
+    
+
+}
+        });
+        memorizedScollAnimation(position,target , onUpdate)
+    }, []);
+
+
+    useEffect(() => {
+        setupViewer();
+
+    }, [])
+    return (
+        <div id='webgi-canvas-container'>
+            <canvas id='webgi-canvas' ref={canvasRef} />
+
+        </div>
+    )
+}
+
+export default WebgiViewer
